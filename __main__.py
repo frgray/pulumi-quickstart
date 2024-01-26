22193b509f7b5e126b25eb5e97942ff4641a87c7
Francisco Gray <frgray@gmail.com>
2024-01-26 07:17:23 -0800

    Removes stacks, adds in usage of network module

--- __main__.py
+++ __main__.py
@@ -1,8 +1,20 @@
-"""An AWS Python Pulumi program"""
-
-from pulumi import StackReference
+import pulumi
 import pulumi_aws as aws
 
-pul
-s3 = StackReference('stacks/instructure/aws/s3')
-vpc = StackReference('stacks/instructure/aws/vpc')
+from network.vpc import SharedVPC
+
+
+config = pulumi.Config()
+vpc_name = config.require('vpc_name')
+cidr_block = config.require('vpc_cidr_block')
+
+azs = aws.get_availability_zones(state="available")
+
+created_vpc = SharedVPC(name=vpc_name,
+                        availability_zones=azs.names[0:3],
+                        cidr_block=cidr_block
+                        )
+
+pulumi.export('vpc_id', created_vpc.vpc.id)
+# pulumi.export('route_tables', created_vpc.route_tables["private"])
+# pulumi.export('private_subnet_ids', created_vpc.subnets["private"])
